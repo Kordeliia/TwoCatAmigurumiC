6763916a16506b3bc1cb4cde704e40bb5be49baf
Kordeliia <lalvmun@hotmail.com>
2023-10-22 14:39:48 +0200

    commit notificaciones

--- app/src/main/java/com/example/twocatsamigurumi/fcm/FCMService.kt
+++ app/src/main/java/com/example/twocatsamigurumi/fcm/FCMService.kt
@@ -1,11 +1,25 @@
 package com.example.twocatsamigurumi.fcm
 
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.os.Build.VERSION_CODES
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import androidx.preference.PreferenceManager
 import androidx.core.content.edit
 import com.example.twocatsamigurumi.Constants
+import com.example.twocatsamigurumi.R
+import com.example.twocatsamigurumi.product.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class FCMService: FirebaseMessagingService() {
     override fun onNewToken(newToken: String) {
@@ -20,6 +34,39 @@ class FCMService: FirebaseMessagingService() {
                 .apply()
         }
         Log.i("abc", newToken)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        remoteMessage.notification?.let{
+            sendNotification(it)
+        }
+    }
+
+
+    private fun sendNotification(notification: RemoteMessage.Notification){
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0,
+            intent, PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE)
+        val channelID = getString(R.string.notification_channel_id_default)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelID)
+            .setSmallIcon(R.drawable.ic_stat_name)
+            .setContentTitle(notification.title)
+            .setContentText(notification.body)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+        val notificationManager = getSystemService(Context
+            .NOTIFICATION_SERVICE) as NotificationManager
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(channelID,
+                getString(R.string.notification_channel_name_default),
+                NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(0, notificationBuilder.build())
     }
 
 }
